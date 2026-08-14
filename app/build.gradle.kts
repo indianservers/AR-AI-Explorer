3e732484b3e69302c8dc9e3be0e9f9a5b9df11bf
indianservers <saisatish@indianservers.com>
2026-08-14 19:27:13 +0530

    Bump release version to 1.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.indianservers.aiexplorer"
         minSdk = 31
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.1.3"
+        versionCode = 14
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
